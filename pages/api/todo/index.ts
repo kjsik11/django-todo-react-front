39f9a0d7165b70856e196475177a9455116da61e
jongsik <kjsik11@gmail.com>
2021-07-01 13:17:52 +0900

    add next config cors setting

--- pages/api/todo/index.ts
+++ pages/api/todo/index.ts
@@ -1,30 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { withErrorHandler } from '@utils/with-error-handler';
 import { connectMongo, throwError } from '@utils/common';
-import Cors from 'cors';
-
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { db } = await connectMongo();
-
-  await runMiddleware(req, res, cors);
 
   if (req.method === 'GET') {
     const cursor = db.collection('todo').aggregate([
@@ -49,8 +28,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     ]);
 
     const todoList = await cursor.toArray();
-
-    console.log(todoList);
 
     return res.json({ todoList });
   }
