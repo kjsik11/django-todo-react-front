014ff68c21cb026a5058a4555b54a5cfe88562ef
jongsik <kjsik11@gmail.com>
2021-07-29 22:12:14 +0900

    d

--- defines/cookie.ts
+++ defines/cookie.ts
@@ -3,7 +3,7 @@ export const COOKIE_KEY_REDIRECT_URL = 'jongsik.redirectUrl';
 
 export const defaultCookieOptions = {
   path: '/',
-  sameSite: 'None',
+  sameSite: 'none',
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
 } as const;
